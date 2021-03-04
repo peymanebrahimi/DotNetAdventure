1db8f82e121cafe970279e0fe3c68e06ae45d9e3
peymanebrahimi <peyman.ebrahimi@gmail.com>
2021-03-04 19:03:16 +0330

    Aggregation and builder

---
+++ DesignPattern/DesignPattern.ConsoleApp1/AggregationVsComposition.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPattern.ConsoleApp1
+{
+    // aggregation vs composition
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+
+        public Employee(string name, Address add)
+        {
+            Name = name;
+            Address = add;
+        }
+    }
+
+    public class Address
+    {
+        public string HouseNumber { get; set; }
+        public string Street { get; set; }
+    }
+
+
+
+    public class CEO
+    {
+        //CEO implementation   
+    }
+
+    public class Organization
+    {
+        public List<Employee> Employees { get; set; }
+        public CEO CEO { get; set; }
+
+        public void AddEmployee(Employee emp)
+        {
+            if (emp != null) Employees.Add(emp);
+        }
+
+    }
+}
---
+++ DesignPattern/DesignPattern.ConsoleApp1/ScottBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DesignPattern.ConsoleApp1
+{
+    public class Address2
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public bool IsVip { get; set; }
+        public Address2 Address { get; set; }
+    }
+
+    public class Account
+    {
+        public DateTime DueDate { get; set; }
+        public Customer Customer { get; set; }
+        public double Balance { get; set; }
+
+    }
+    //////////////////////////////////////////////////
+
+    public class AccountBuilder
+    {
+        private Account _account;
+
+        public AccountBuilder()
+        {
+            _account = new Account()
+            {
+                Balance = 10000,
+                DueDate = DateTime.Now.AddDays(1),
+                Customer = new Customer()
+                {
+                    IsVip = false,
+                    Name = "Scott",
+                    Address = new Address2()
+                    {
+                        City = "LA",
+                        Country = "USA"
+                    }
+                }
+            };
+        }
+
+        public static AccountBuilder DefaultAccount()
+        {
+            return new AccountBuilder();
+        }
+
+        public AccountBuilder WithLatePaymentStatus()
+        {
+            _account.DueDate = DateTime.Now.AddDays(-1);
+            return this;
+        }
+
+        public AccountBuilder WithVipCustomer()
+        {
+            _account.Customer.IsVip = true;
+            return this;
+        }
+
+        public Account Build()
+        {
+            return _account;
+        }
+    }
+}
